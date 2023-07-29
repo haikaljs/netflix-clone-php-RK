3dad9deac0ce951ca3636de3976ba137d998dfe3
haikaljs <haikaljavascript@gmail.com>
2023-07-29 23:44:44 +0800

    Create FormSanitizer class

---
+++ includes/classes/FormSanitizer.php
@@ -0,0 +1,15 @@
+<?php
+
+class FormSanitizer{
+    public static function sanitizeFormString($inputText){
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        // $inputText = trim(" ", "", $inputText);
+        $inputText = strtolower($inputText);
+        $inputText = ucfirst($inputText);
+
+        return $inputText;
+    }
+}
+
+?>
