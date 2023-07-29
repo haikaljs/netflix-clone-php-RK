466baa02438b57462d05292d33effad9e0a77ddc
haikaljs <haikaljavascript@gmail.com>
2023-07-29 18:25:04 +0800

    Creating register page

--- register.php
+++ register.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome To Netflix</title>
+    <link rel="stylesheet" type="text/css" href="./assets/styles.css">
 </head>
 <body>
-    
+    Register
 </body>
 </html>
