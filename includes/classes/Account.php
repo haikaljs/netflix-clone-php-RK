4f5246bbf3eaffba9e76fce3a176b76019f642da
haikaljs <haikaljavascript@gmail.com>
2023-07-30 04:50:59 +0800

    Validating firstName

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -3,9 +3,18 @@
 class Account{
 
     private $con;
+    private $errorArray;
+
+
     public function __construct($con)
     {
         $this->con = $con;
+    }
+
+    public function validateFirstName($fn){
+        if(strlen($fn) < 2 || strlen($fn) > 25){
+            array_push($this->errorArray, "First name wrong length");
+        }
     }
 }
 
