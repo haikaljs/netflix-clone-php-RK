21f5d20abbd702f1839c2368d81654306bef1f2e
haikaljs <haikaljavascript@gmail.com>
2023-07-29 18:52:33 +0800

    Creating input fields

--- register.php
+++ register.php
@@ -9,7 +9,16 @@
 <body>
     <div class="signInContainer">
         <div class="column">
-            
+            <form method="POST">
+                <input type="text" name="firstName" placeholder="First name" required>
+                <input type="text" name="lasttName" placeholder="Last name"  required>
+                <input type="text" name="userName" placeholder="Username"  required>
+                <input type="email" name="email" placeholder="Email"  required>
+                <input type="email" name="email2" placeholder="Confirm email"  required>
+                <input type="password" name="password" placeholder="Password"  required>
+                <input type="password" name="password2" placeholder="Confirm Password"  required>
+                <input type="submit" name="submitPassword" value="Submit">
+            </form>
         </div>
     </div>
 </body>
