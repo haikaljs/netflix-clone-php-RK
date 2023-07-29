36cb8f0e0d8a7b1fd37e220c286821c56305af22
haikaljs <haikaljavascript@gmail.com>
2023-07-30 04:21:03 +0800

    Connecting to database

--- includes/config.php
+++ includes/config.php
@@ -4,4 +4,13 @@ session_start(); // By default session will last when browser close
 
 date_default_timezone_set("Asia/Kuala_Lumpur");
 
+try{
+
+    $con = new PDO("msql:dbname=netflix;host=localhost", "root", "root");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+}catch(PDOException $e){
+    exit("Connection failed: " . $e->getMessage());
+}
+
 ?>
